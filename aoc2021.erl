19775ea999f1ef8762650575065ad19dff37e627
Brian Ward <bward@vhtcx.com>
2021-12-11 06:29:03 -0500

    added day4 to the aoc2021 module

--- aoc2021.erl
+++ aoc2021.erl
@@ -3,7 +3,8 @@
 -export([
 	 day1/0,day1b/0,
 	 day2/0,day2b/0,
-	 day3/0,day3b/0
+	 day3/0,day3b/0,
+	 day4/0,day4b/0
 	]).
 
 day1() ->
@@ -23,3 +24,9 @@ day3() ->
 
 day3b() ->
 	day3:day3b().
+
+day4() ->
+	day4:day4().
+
+day4b() ->
+	day4:day4b().
